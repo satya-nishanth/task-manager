40d4125f742f5e767443e900559183bdc77f3790
satya nishanth kondamuri <nishi@Nishi-Mac.local>
2024-05-07 01:22:44 +0530

    chnages

--- backend/src/model/task.ts
+++ backend/src/model/task.ts
@@ -2,7 +2,7 @@ import mongoose, { Model, Schema, model } from "mongoose";
 import { ITask } from "utils/constants";
 
 const taskSchema = new Schema<ITask>({
-    title: { type: "String", required: true,unique: true },
+    title: { type: "String", required: true },
     status: { type: "String",required: true, enum: ["to_do", "in_progress", "done"], default: "to_do" },
     description: { type: "String", required: false },
     user:{ type: Schema.Types.ObjectId, ref: 'User', required: true }
